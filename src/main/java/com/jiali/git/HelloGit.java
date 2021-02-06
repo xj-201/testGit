0e86eaa54c6aff6f451d76fccfbeb4334a19f9fa
xj-201 <xj-201@hotmail.com>
2021-02-06 15:08:32 +0800

    添加文件

--- src/main/java/com/jiali/git/HelloGit.java
+++ src/main/java/com/jiali/git/HelloGit.java
@@ -13,5 +13,7 @@ package com.jiali.git;
 public class HelloGit {
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
+		System.out.println("Hello Git!");
+		System.out.println("Hello Git!");
 	}
 }
