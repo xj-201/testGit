791e2575de333c696e770298a1f9f28ff59a2331
xj-201 <xj-201@hotmail.com>
2021-02-06 15:10:38 +0800

    更新1

--- src/main/java/com/jiali/git/HelloGit.java
+++ src/main/java/com/jiali/git/HelloGit.java
@@ -13,7 +13,6 @@ package com.jiali.git;
 public class HelloGit {
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
-		System.out.println("Hello Git!");
-		System.out.println("Hello Git!");
+		System.out.println("更新!");
 	}
 }
