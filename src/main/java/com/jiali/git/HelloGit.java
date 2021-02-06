42054b3028ae9ae1e08be64310b93435f3851a5b
xj-201 <xj-201@hotmail.com>
2021-02-06 15:05:53 +0800

    添加文件

---
+++ src/main/java/com/jiali/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.jiali.git;
+
+/**
+ * @Description
+ * @author ishiali Email:ishiali@outlook.com
+ * @version
+ * @date 2021年2月6日下午2:27:49
+ */
+
+public class HelloGit {
+	public static void main(String[] args){
+		System.out.println("Hello Git!");
+	}
+}
